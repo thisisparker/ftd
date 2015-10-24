0c96e716eaf89351a9ed08ae96bc591ca6192fec
Parker Higgins <parker@parkerhiggins.net>
2015-10-24 16:31:48 -0700

    added security

--- getobits.py
+++ getobits.py
@@ -5,7 +5,7 @@ import os, requests, json
 
 nyt_api_key = os.environ["NYT_API_KEY"]
 
-api_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=type_of_material:%28%22Obituary%22%29&sort=newest&fl=headline,web_url,snippet,pub_date&api-key=" + nyt_api_key
+api_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=type_of_material:%28%22Obituary%22%29&sort=newest&fl=headline,web_url,snippet,pub_date&api-key=" + nyt_api_key
 
 res = requests.get(api_url)
 res.raise_for_status()
