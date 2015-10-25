c5224d2a00c5ec557bc002eb14fcc2603815e76a
Parker Higgins <parker@parkerhiggins.net>
2015-10-24 17:20:22 -0700

    plugging in variables and creating sample requests

--- getobits.py
+++ getobits.py
@@ -1,7 +1,8 @@
 #! python3
-# currently prints the last 10 NYT obit headlines. But more is to come.
+# Grabs the last 10 NYT obits and prepares the text of a FOIA request for their FBI files.
 
-import os, requests, json
+import os, requests, json, datetime, re
+from datetime import datetime
 
 nyt_api_key = os.environ["NYT_API_KEY"]
 
@@ -15,4 +16,10 @@ api_results = json.loads(res.text)
 docs = api_results['response']['docs']
 
 for i in docs:
-    print(i['headline']['main'])
+    obit_source = "The New York Times" # May be more sources in the future, for now just NYT.
+    obit_headline = i['headline']['main']
+    obit_date = datetime.strftime(datetime.strptime(i['pub_date'],"%Y-%m-%dT%H:%M:%SZ"),"%B %d, %Y") # Dates are annoying, right? This line converts NYT's ISO formatted pub_date to a human-readable format.
+    dead_person = re.match("[^,]*",obit_headline).group() # guesses the name of the person by the headline up until the comma. Brittle, but matches NYT syntax without fail so far.
+    obit_URL = i['web_url']
+
+    print("A copy of all documents or FBI files pertaining to " + dead_person + ", an obituary of whom was published in " + obit_source + " on " + obit_date + " under the headline \"" + obit_headline + "\" and can be found at " + obit_URL + ".")
